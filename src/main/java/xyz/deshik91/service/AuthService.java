9e8902ed2a2c92859ba8984f55ff8e57d9a86751
deshik91 <asatrian03@bk.ru>
2026-02-18 15:01:20 +0300

    fix: исправлены иморты зависимостей в AuthService.java

--- src/main/java/xyz/deshik91/service/AuthService.java
+++ src/main/java/xyz/deshik91/service/AuthService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import xyz.deshik91.dto.request.LoginRequest;
+import xyz.deshik91.dto.request.RefreshTokenRequest;
 import xyz.deshik91.dto.request.RegisterRequest;
 import xyz.deshik91.dto.response.AuthResponse;
 import xyz.deshik91.model.Invitation;
